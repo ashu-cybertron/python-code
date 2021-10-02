dc8578246c325a8ad4e48fc07800c5c5a9c02a9c
Raghav Gupta <raggupta@raggupta.pnq.csb>
2021-10-02 20:31:00 +0530

    updated: factorial code updated

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#factorial of a number
+
+def cal_factorial(num: int):
+
+    fact=1
+    if num<0:
+        print("factorial of negative number is not possible")
+    elif num==0:
+        print('factorial of zero is 1')
+    elif num>0:
+        for i in range(num,1,-1):
+            fact=fact*i
+    return fact 
+
+if __name__ == '__main__': 
+
+print(cal_factorial(4)) 
